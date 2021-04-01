32d6f9c15c814252faf8c58008fa05f9783f578a
Alfredoej <alfredo.ej@gmail.com>
2021-04-01 06:08:25 -0400

    inicio aula 02

--- src/routes.js
+++ src/routes.js
@@ -17,9 +17,47 @@ const profile ={
      "vacation-per-year" : 4
 
 }
+
+//array que irá receber os objetos de jobs cadastrados na rodat job
+const jobs = [
+{
+     id: 1,
+     name:"Pizzaria Guloso",
+     "daily-hours": 2,
+     "total-hours": 30,
+     created_at: Date.now()   
+},
+{
+     id: 2,
+     name:"Hamburguer Ogro",
+     "daily-hours": 3,
+     "total-hours": 50,
+     created_at: Date.now()   
+}
+
+];
+
 //request, response
-routes.get('/', (req, res) => res.render(views + "index"));
+routes.get('/', (req, res) => res.render(views + "index", { jobs }));
 routes.get('/job', (req, res) => res.render(views + "job"));
+
+routes.post('/job', (req, res) => {
+//{ name: 'Alfredo', 'daily-hours': '5', 'total-hours': '20' }
+     
+     const lastId = jobs[jobs.length-1]?.id || 1; // o ?é um escape para caso não exsita o id, no caso o primeiro
+
+     jobs.push({
+          id: lastId + 1,
+          name: req.body.name,
+          "daily-hours": req.body["daily-hours"],
+          "total-hours": req.body["total-hours"],
+          created_at: Date.now() //atribuindo uma nova data
+
+     });
+
+     return res.redirect('/');
+});
+
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile }));
 
